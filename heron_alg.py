836def43507de4ce10e62e054ce1d270354b43e1
Jonah <82569523+Jonah987654321@users.noreply.github.com>
2023-11-22 13:16:59 +0100

    Code optimization + bug fixes

--- heron_alg.py
+++ heron_alg.py
@@ -15,23 +15,13 @@ while num_float is False or float(num) <= 0:
     print("Invalid input. Please insert number!")
     num = input("Enter the number from which the square root should be formed: ")
     num_float = isnumber(num, "float")
-x = input("Enter accuracy (how often should the method be used?): ")
-accuracy_int = isnumber(x, "int")
-while accuracy_int is False or int(x) <= 0:
-    print("Invalid input! Please insert natural number!")
-    x = input("Enter accuracy (how often should the method be used?): ")
-    accuracy = isnumber(x, "int")
-x = int(x)
+
 a = float(num)
 num = float(num)
-for r in range(1, x, 1):
+a_last = float(a)
+a = (a_last + (num / a_last)) / 2
+while a != a_last:
     a_last = float(a)
-    a = (a_last + (zahl / a_last)) / 2
-    if a == a_last:
-        break
-y = a*a
-approximation = abs(y-num)/num
-approximation = approximation*100
-approximation = round(approximation, 2)
-print(f"\n\n\nThe computed approximation from the square root of {num} is {a}.")
-print(f"NOTE: The square of the calculated approximation deviates by {approximation}% from the entered number!")
+    a = (a_last + (num / a_last)) / 2
+
+print(f"\n\n\nThe computed approximation from the square root of {num} is {a}")
